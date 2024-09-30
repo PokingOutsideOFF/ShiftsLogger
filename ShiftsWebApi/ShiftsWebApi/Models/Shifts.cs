9df6a973456cf157e68f1e7147764e16920809ca
KaitoKid <shresawnt2011@gmail.com>
2024-09-30 20:43:34 +0530

    Initial Commit

---
+++ ShiftsWebApi/ShiftsWebApi/Models/Shifts.cs
@@ -0,0 +1,11 @@
+﻿namespace ShiftsWebApi.Models
+{
+    public class Shifts
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
---
+++ ShiftsWebApi/ShiftsWebApi/Models/ShiftsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShiftsWebApi.Models
+{
+    public class ShiftsContext: DbContext
+    {
+        public DbSet<Shifts> Shifts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
